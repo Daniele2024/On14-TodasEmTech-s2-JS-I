edadc6278fe3cd07cef220443e24389477d78e17
daniele <danivilarez@hotmail.com>
2021-08-28 19:39:14 -0300

    atividade concluida

---
+++ Conteudo1-keila/Conteudo1/keylinha.js
@@ -0,0 +1,8 @@
+alert("Seja bem vinda ao meu site ");
+alert("vamos somar 87 + 96 =" + ( 87+96));
+alert("vamos multiplicar 5 e 8 =" + ( 5*8));
+
+var listaDeCompras = ['arroz', "feijao","carne"]
+
+alert(listaDeCompras)
+
